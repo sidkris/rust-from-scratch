f7092e324757dbc65fa5d31a9cff5e422456cd38
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-08-15 20:04:07 +0530

    name

--- foundation/src/main.rs
+++ foundation/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Please enter your name: ");
+
+    let mut name = String::new();
+
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Failed to read line");
+
+    let name = name.trim();
+
+    // Print a greeting message
+    println!("Hello there, {}!", name);
+
 }
+
