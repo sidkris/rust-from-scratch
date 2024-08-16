ea9ec976793d6b3419e0a4dd8a38bd1030b2e828
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-08-16 23:10:41 +0530

    updates

--- foundation/src/main.rs
+++ foundation/src/main.rs
@@ -6,8 +6,17 @@ fn main(){
     height_check(186);
     age_check(30);
     while_loop();
+    for_loop();
+    for_loop_2();
+    match_greetings("Hello");
+    simple_unit_function(&[1, 2, 3, 4, 5]);
+    
+    let chunks = split_strings("Hello, World".to_string(), ',', 0);
+    println!("Split String : {}", chunks);
+
 }
 
+// ==========================================================================
 
 fn print_name() {
 
@@ -25,6 +34,7 @@ fn print_name() {
 
 }
 
+// ==========================================================================
 
 fn height_check(height : i32){
 
@@ -40,8 +50,9 @@ fn height_check(height : i32){
         println!("hey, a normal human being! ;)")
     }
 
-
 }
+
+// ==========================================================================
 
 fn age_check(age : i32){
 
@@ -60,3 +71,68 @@ fn while_loop(){
     }
 
 }
+
+// ==========================================================================
+
+fn for_loop(){
+
+    for i in 11..21 {
+        println!("i = {}", i);
+    }
+
+}
+
+// ==========================================================================
+
+fn for_loop_2(){
+
+    let numbers = vec![101, 102, 103, 104, 105];
+
+    for number in numbers{
+        println!("{}", number);
+    }
+
+}
+
+// ==========================================================================
+
+fn match_greetings(greeting : &str){
+
+    match greeting{
+
+        "Hello" => println!("Hello there!"),
+        "Bye" => println!("Talk to you later!"),
+        _ => println!("Sorry, could you please say that again?"),
+
+    }
+
+}
+
+// ==========================================================================
+
+// Unit Functions do not return any value(s)
+fn simple_unit_function(numbers : &[i32]){
+
+    let mut sum = 0;
+
+    for number in numbers{
+        sum += number;
+    }
+
+    println!("The sum of the numbers is {}", sum);
+
+}
+
+// ==========================================================================
+
+fn split_strings(input_string : String, delimiter : char, chunk_index : usize) -> String {
+
+    let parts : Vec<&str> =  input_string.split(delimiter).collect();
+    let result : Option<&&str> = parts.get(chunk_index);
+
+    result.expect("Oops, that didn't work..!").to_string()
+    
+}
+
+// ==========================================================================
+
