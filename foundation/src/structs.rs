4edf2d3c5203db33782ca21f5f898e3c3a67d701
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-08-22 22:50:46 +0530

    structs.rs

--- foundation/src/structs.rs
+++ foundation/src/structs.rs
@@ -5,6 +5,16 @@ struct Person{
     age : u8,
 }
 
+impl Person{
+    fn new(name : String, age : u8) -> Person {
+    Person{
+            name,
+            age,
+        }
+    }
+}
+
+
 fn main() {
 
     let sid = Person{
@@ -13,5 +23,13 @@ fn main() {
     };
 
     println!("{} is {} years old.", sid.name, sid.age);
+
+    let new_person = Person::new(
+        "Sid Krishnan".to_string(),
+        34
+    );
+
+    println!("{} is {} years old.", new_person.name, new_person.age);
+
 
 }
