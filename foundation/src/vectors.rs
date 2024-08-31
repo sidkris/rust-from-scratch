90d14147506d0ae11d436f4cee3a31e359c44091
Siddharth Krishnan <sidkrishnan@protonmail.com>
2024-08-31 13:19:55 +0530

    vectors

--- foundation/src/vectors.rs
+++ foundation/src/vectors.rs
@@ -9,4 +9,16 @@ fn main() {
     let num_slice = &nums[0..2];
     println!("{:?}", num_slice);
 
+    mutable_vec();
+
+}
+
+
+fn mutable_vec(){
+
+    let mut num_vec = vec![1, 2, 3];
+
+    num_vec.extend([4, 5, 6, 7, 8, 9, 10]);
+
+    println!("New Vec : {:?}", num_vec)
 }
